bf8299aa7732c5be5708f94ccd4e4d8ea4de0419
MathiasDanzeisen <matze@specialo.de>
2021-10-02 17:01:24 +0200

    imporved system

--- src/system.cpp
+++ src/system.cpp
@@ -2,6 +2,7 @@
 
 #include <unistd.h>
 
+#include <algorithm>
 #include <cstddef>
 #include <set>
 #include <string>
@@ -19,24 +20,36 @@ using std::vector;
 // Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
+// Return a container composed of the system's processes
 vector<Process>& System::Processes() {
   auto pids = LinuxParser::Pids();
+  processes_.clear();
   for (int pid : pids) {
     processes_.push_back(Process(pid));
   }
-
+  std::sort(processes_.begin(), processes_.end());
   return processes_;
 }
 
 // Return the system's kernel identifier (string)
-std::string System::Kernel() { return LinuxParser::Kernel(); }
+std::string System::Kernel() {
+  if (this->kernel_.empty()) {
+    kernel_ = LinuxParser::OperatingSystem();
+  }
+  return kernel_;
+}
 
 // Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
 // Return the operating system name
-std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
+std::string System::OperatingSystem() {
+  if (this->os_.empty()) {
+    os_ = LinuxParser::OperatingSystem();
+  }
+
+  return os_;
+}
 
 // Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
